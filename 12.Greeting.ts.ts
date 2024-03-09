7fcad581f2635cdcfde4f51d936c42fbafb29943
KHURRAM <160718125+khurramSunny@users.noreply.github.com>
2024-03-09 19:20:49 +0500

    Add files via upload

---
+++ 12.Greeting.ts.ts
@@ -0,0 +1,9 @@
+let Names :string[] = ['Khurram' , 'Ishtiaq' , 'Abdul Rafay' , 'Abdul Samad'] ;
+
+const message :string = 'Welcome to Typescript Class: '
+
+for (let i=0; i<Names.length; i++) 
+
+{
+    console.log(message + Names[i]);
+}
