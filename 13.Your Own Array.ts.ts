6f05ad0136bf1960a9d4e6234d263b1c70b7b88d
KHURRAM <160718125+khurramSunny@users.noreply.github.com>
2024-03-09 19:21:27 +0500

    Add files via upload

---
+++ 13.Your Own Array.ts.ts
@@ -0,0 +1,7 @@
+
+
+let transportation :string[] = ['cycle','Motorcycle','Car','Train'] ;
+
+for (let i=0; i<transportation.length; i++){
+    console.log('I would like to own a '+ transportation[i])
+}
