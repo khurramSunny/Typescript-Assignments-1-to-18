d32374abd6634650c295dc485cac736053ac6ae4
KHURRAM <160718125+khurramSunny@users.noreply.github.com>
2024-03-09 19:22:00 +0500

    Add files via upload

---
+++ 14.Guest List.ts.ts
@@ -0,0 +1,7 @@
+let Guest_List :string[] = ['Imran Khan ','Nawaz Sharif ','Asif Ali Zardari '];
+
+for(let i=0; i<Guest_List.length; i++){
+
+console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\nThankyou! \n\n')
+
+}
